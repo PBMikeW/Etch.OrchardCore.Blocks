6c10a719b5702b7d68737778873175f96981377f
Mike Williamson <89110166+PBMikeW@users.noreply.github.com>
2024-10-04 10:06:21 +1300

    Update ListBlockParser.cs

--- EditorJS/Parsers/Blocks/ListBlockParser.cs
+++ EditorJS/Parsers/Blocks/ListBlockParser.cs
@@ -1,5 +1,9 @@
 ﻿using Etch.OrchardCore.Blocks.EditorJS.Parsers.Models;
 using Etch.OrchardCore.Blocks.ViewModels.Blocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
@@ -9,10 +13,12 @@ namespace Etch.OrchardCore.Blocks.EditorJS.Parsers.Blocks
     {
         public async Task<dynamic> RenderAsync(BlockParserContext context, Block block)
         {
+            List<Object> itemList = (List<Object>)block.Data["items"];
+            var items = itemList.Select(item => item?.ToString() ?? "").ToArray();
             return await context.ShapeFactory.New.Block__List(
                 new ListBlockViewModel
                 {
-                    ListItems = block.Has("items") ? (block.Data["items"] as JsonArray).ToObject<string[]>() : new string[0],
+                    ListItems = block.Has("items") ? items : new string[0],
                     Style = block.Get("style")
                 }
             );
